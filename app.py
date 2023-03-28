fa32382dc3d9e9133ecf6a1a3bc754688b98201d
Gustavo Correa <38304334+gustavohmc@users.noreply.github.com>
2023-03-27 22:46:16 -0700

    implemented list and delete

--- app.py
+++ app.py
@@ -15,39 +15,17 @@ def menu():
     user_input = input(USER_CHOICE)
     while user_input != 'q':
         if user_input == 'a':
-            add_book()
+            database.add_book()
         elif user_input == 'l':
-            list_all_books()
+            database.list_all_books()
+            user_input = input(USER_CHOICE)
         elif user_input == 'r':
-            mark_as_read()
+            database.mark_as_read()
+            user_input = input(USER_CHOICE)
         elif user_input == 'd':
-            delete_book()
+            database.delete_book()
         elif user_input == 'q':
             exit()
-        menu()
-
-
-def add_book():
-    pass
-
-
-def list_all_books():
-    pass
-
-
-def mark_as_read():
-    book_to_update = input('Which book should I mark as read? ')
-    for book in database.books:
-        if book['name'] == book_to_update:
-            book['read'] = True
-            print(f'Book {book_to_update} was marked as read')
-            menu()
-    print(f'There is no book named {book_to_update} in the database')
-    menu()
-
-
-def delete_book():
-    pass
 
 
 menu()
--- utils/database.py
+++ utils/database.py
@@ -1,3 +1,5 @@
+import utils.database
+
 books = [
     {
         'name': 'Foundation',
@@ -11,3 +13,31 @@ books = [
     }
 ]
 
+
+def add_book():
+    pass
+
+
+def list_all_books():
+    for book in books:
+        print(f'Title: {book["name"]}, Author: {book["author"]}, Read: {"Yed" if book["read"] else "No"}')
+
+
+def mark_as_read():
+    book_to_update = input('Which book should I mark as read? ')
+    for book in books:
+        if book['name'] == book_to_update:
+            if book['read']:
+                print('This book is already marked as read')
+            else:
+                book['read'] = True
+                print(f'Book {book_to_update} was marked as read')
+            return
+
+    else:
+        print(f'There is no book named {book_to_update} in the database')
+
+
+def delete_book():
+    book_to_delete = input('Which book would you like to delete?')
+    utils.database.books = [book for book in utils.database.books if book['name'] != book_to_delete]
